07539b6e38b52e1f2a1537c634deafb38dbe9920
Fikri Abdul Rahman <fikriabr96@gmail.com>
2026-02-21 09:15:47 +0700

    Move header to new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,10 @@
-import { MapIcon, TruckIcon } from '@heroicons/react/24/outline'
 import Dropdown from './components/Dropdown'
+import Header from './components/Header'
 
 function App() {
-  const menu = [
-    { text: 'Vehicles', url: '/', icon: TruckIcon },
-    { text: 'View on Map', url: '/map', icon: MapIcon },
-  ]
   return (
     <>
-      <header className="fixed h-[100px] bg-[#06367c] w-full flex items-center justify-center">
-        <div className="container mx-6 lg:mx-0">
-          <nav className="flex gap-16">
-            <a href="/" className="outline-none">
-              <img alt="" src="/icon.png" className="w-[74px] h-auto" />
-            </a>
-            <div className="flex flex-1 items-center justify-end">
-              <ul className="flex gap-4 text-white text-md font-bold">
-                {menu.map(({ icon: MenuIcon, text, url }) => (
-                  <li>
-                    <a
-                      className="px-6 py-3 hover:bg-slate-100/10 rounded-lg flex gap-2"
-                      href={url}
-                    >
-                      <MenuIcon className="size-6" />
-                      {text}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </nav>
-        </div>
-      </header>
+      <Header />
       <section className="pt-[100px] w-full h-screen">
         <div className="h-full w-full flex items-center justify-center p-6">
           <div className="h-full w-full rounded-xl flex flex-col">
@@ -39,26 +12,18 @@ function App() {
               id="filter"
               className="h-fit border-b flex flex-col gap-4 p-4 bg-slate-300 rounded-t-xl"
             >
-              <div className="flex gap-3 w-full items-center">
+              <div className="flex gap-3 w-full items-center flex-col lg:flex-row">
                 <div className="text-xs uppercase font-bold text-gray-600">
                   Filter
                 </div>
-                {/* <input
-                  name="dropdown-route"
-                  className="h-[40px] px-4 rounded-lg w-full"
-                  placeholder="Dropdown Route"
-                />
-                <input
-                  name="dropdown-trips"
-                  className="h-[40px] px-4 rounded-lg w-full"
-                  placeholder="Dropdown Trip"
-                /> */}
+                <div className="flex w-full gap-4">
+                  <Dropdown />
+                  <Dropdown />
+                </div>
 
-                <Dropdown />
-                <Dropdown />
                 <button
                   type="button"
-                  className="rounded-lg bg-[#06367c] hover:bg-[#06367c]/20 px-8 py-2 text-white font-semibold"
+                  className="text-[13px] rounded-lg bg-[#06367c] hover:bg-[#06367c]/20 px-8 py-2 text-white font-semibold"
                 >
                   Search
                 </button>
@@ -71,7 +36,10 @@ function App() {
               id="contents"
               className="h-full bg-slate-100 rounded-b-xl p-4 overflow-y-auto relative flex flex-col gap-4"
             >
-              <div id="contents-s" className="grid grid-cols-4 gap-4 h-fit">
+              <div
+                id="contents-s"
+                className="grid grid-cols-1 lg:grid-cols-4 gap-4 h-fit"
+              >
                 <div className="w-full h-[250px] bg-gray-200 rounded-lg"></div>
                 <div className="w-full h-[250px] bg-gray-200 rounded-lg"></div>
                 <div className="w-full h-[250px] bg-gray-200 rounded-lg"></div>
@@ -87,7 +55,10 @@ function App() {
 
             <div className="flex gap-2 justify-center pt-2">
               {['Prev', '1', '2', 'Next'].map((item) => (
-                <button className="p-1.5 px-4 bg-gray-400 text-white rounded-md">
+                <button
+                  key={item}
+                  className="p-1.5 px-4 bg-gray-400 text-white rounded-md"
+                >
                   {item}
                 </button>
               ))}
---
+++ src/components/Header.tsx
@@ -0,0 +1,36 @@
+import { MapIcon, TruckIcon } from '@heroicons/react/24/outline'
+
+const Header = () => {
+  const menu = [
+    { text: 'Vehicles', url: '/', icon: TruckIcon },
+    { text: 'View on Map', url: '/map', icon: MapIcon },
+  ]
+  return (
+    <header className="fixed h-[100px] bg-[#06367c] w-full flex items-center justify-center">
+      <div className="container mx-6 lg:mx-0">
+        <nav className="flex gap-16">
+          <a href="/" className="outline-none">
+            <img alt="" src="/icon.png" className="w-[74px] h-auto" />
+          </a>
+          <div className="hidden lg:flex flex-1 items-center justify-end">
+            <ul className="flex gap-4 text-white text-md font-bold">
+              {menu.map(({ icon: MenuIcon, text, url }) => (
+                <li key={text}>
+                  <a
+                    className="px-6 py-3 hover:bg-slate-100/10 rounded-lg flex gap-2"
+                    href={url}
+                  >
+                    <MenuIcon className="size-6" />
+                    {text}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Header
